d637cf86c28d40adb9cadab7526989a6ad30739d
Juan Camilo <caamilo9517@gmail.com>
2023-01-04 11:36:28 -0500

    Todo listo, sólo falta lo de copiar en el portapapeles

--- js/index.js
+++ js/index.js
@@ -5,16 +5,46 @@ import { desencriptar } from "./desencriptar.js";
 const $encriptar=document.getElementById("encriptar"),
       $desencriptar=document.getElementById("desencriptar"),
       $copiar=document.getElementById("copiar");
+//Capturo el campo de texto donde el usuario pone el mensaje.
+const $textArea=document.getElementById("mensaje");
+const $parrafo=document.getElementById("mensaje-encriptado");
+let msg="";
+let msgEncriptado="";
 
+//Campuro la información a mostrar en el aside tanto como para éxito como para error.
+const $exito=document.getElementById("exito");
+const $error=document.getElementById("error");
+
+//Acá voy con los eventos click de mi aplicativo.
 document.addEventListener("click",e=>{
     if(e.target.matches("#encriptar")){
-        console.log("encriptando");
-        console.log(encriptar("gato"));
-        console.log(encriptar("perro lobo"));
-    }
+        msg=$textArea.value;
+        $encriptar.classList.add("boton-activo");
+        $desencriptar.classList.remove("boton-activo");
+       if (msg===""){
+            $exito.classList.remove("is-active");
+            $error.classList.add("is-active");
+       }else{
+            $exito.classList.add("is-active");
+            $error.classList.remove("is-active");
+            msgEncriptado=encriptar(msg);
+            $parrafo.textContent=msgEncriptado;
+       }
+      
+    } 
     if(e.target.matches("#desencriptar")){
-        console.log("Desencriptando");
-        console.log(desencriptar("fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!"));
+        msgEncriptado=$textArea.value;
+        $encriptar.classList.remove("boton-activo");
+        $desencriptar.classList.add("boton-activo");
+        if (msgEncriptado===""){
+             $exito.classList.remove("is-active");
+             $error.classList.add("is-active");
+        }else{
+             $exito.classList.add("is-active");
+             $error.classList.remove("is-active");
+             msg=desencriptar(msgEncriptado);
+             $parrafo.textContent=msg;
+        }
        
        
     }
